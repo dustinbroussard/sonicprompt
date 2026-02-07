7f070cc5ab983f30cfd77f85c198ed3b6f5a504e
dustinbroussard <dustin.broussard@gmail.com>
2026-02-07 16:00:07 -0600

    first

---
+++ sw.js
@@ -0,0 +1,72 @@
+const CACHE_VERSION = 'v1.1.0';
+const STATIC_CACHE = `suno-prompter-${CACHE_VERSION}`;
+
+const STATIC_ASSETS = [
+  './',
+  './sunoprompter.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== STATIC_CACHE) {
+            return caches.delete(key);
+          }
+          return null;
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  const url = new URL(request.url);
+
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('./sunoprompter.html').then((cached) =>
+        cached || fetch(request)
+      )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const fetchPromise = fetch(request)
+        .then((response) => {
+          if (response && response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(STATIC_CACHE).then((cache) => cache.put(request, responseClone));
+          }
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || fetchPromise;
+    })
+  );
+});
